d72be0f645f53b15ed33932bc81b79dc59b7bd17
DzmitryMytsko <dzmitrymytsko@gmail.com>
2023-09-16 14:48:38 +0300

    IfElse

---
+++ IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Input name of User: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("Yep, it's Masha!");
+}
+else
+{
+    Console.Write("Hi, ");
+    Console.Write(username);
+}
